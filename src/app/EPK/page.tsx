2ec1bcdc00193793ac484a3120bb1b238e597da9
Kushagra Katiyar <k.katiyar2006@gmail.com>
2026-04-03 12:31:18 -0400

    updated navbar and started epk

--- src/app/EPK/page.tsx
+++ src/app/EPK/page.tsx
@@ -1,13 +1,21 @@
+import Image from "next/image";
 
-
-
-
-export default function EPK(){
+export default function EPK() {
     return (
-        <>
-        <div className="flex items-center justify-center min-h-screen">
-             <h1>EPK</h1>
+        <div className="justify-center min-h-screen">
+            <div className="Hero flex items-center gap-4">
+                <Image 
+                        src="/epk_hero.png"
+                        alt="EPK Hero"
+                        width={1920} 
+                        height={1080} 
+                        className="w-full h-auto"
+                    />
+            </div>
+            
+            <div className = "Main flex-1 w-full px-8 py-16 bg-gray-100">
+                
+            </div>
         </div>
-        </>
-    )
+    );
 }
