159262be6fd322b543605de9a2928151deb5f5c4
kalinatringas <kalinatringas@ufl.edu>
2026-04-11 10:51:39 -0400

    Finished the lyrics page

---
+++ src/app/components/LyricsBox.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { useState } from "react"
+import { Song } from "../data/songData";
+
+
+export default function LyricsBox({title, album, verses}: Song){
+    const [open, setOpen] = useState(false); // toggles if lyrics are opened or closed
+    const [buttonState, setButtonState] = useState("+")
+    const handleClick = () =>{
+        setOpen(!open);
+        if (open === true){
+            setButtonState("+")
+        } else{
+            setButtonState("-")
+        }
+
+    }
+
+    return (<>
+    <div className="flex flex-col border-black border rounded-lg p-4 w-150 m-3">
+        <div className="flex flex-row justify-between items-end ">
+            <div className="text-2xl text-black">{title}</div>
+            <button
+            className="w-10 text-2xl text-black font-bold"
+            onClick={handleClick}
+            >{buttonState}</button>
+        </div>
+        {open && (
+         <div>
+            {verses.map((verse, i)=>(
+                <div key={i}>
+                    {verse.map((line, j)=>(
+                        <p className="text-black" key={j}>{line}</p>
+                    ))}
+                </div>
+            ))}
+         </div>
+        )}
+    </div>
+    
+    </>)
+}
--- src/app/lyrics/page.tsx
+++ src/app/lyrics/page.tsx
@@ -1,13 +1,55 @@
+import Image from "next/image";
 
-
-
+import LyricsHero from "../../../public/LyricsHero.png"
+import ShinyHeadshot from "../../../public/ShinyHeadshot.png"
+import LyricsBox from "../components/LyricsBox";
+import { songs } from "../data/songData";
 
 export default function Lyrics(){
     return (
-        <>
-        <div className="flex items-center justify-center min-h-screen">
-             <h1>Lyrics</h1>
+        <div className="relative min-h-screen">
+            <div className="absolute inset-0 lined-background z-0" />
+            
+            <div className="relative z-10 flex flex-col items-center min-h-screen">
+                <div className="relative w-full flex items-center justify-center" style={{minHeight:"460px"}}>
+                    <Image
+                        src={LyricsHero}
+                        alt="Dreamy image of Alex Willow"
+                        fill
+                        className="object-cover z-0"
+                    />
+                    <div className="absolute inset-0 bg-black opacity-40" style={{zIndex: 1}} />
+                    <div className="relative z-20">
+                        <h1 className="text-white text-4xl">Lyrics</h1>
+                    </div>
+                </div>
+                <div className="flex flex-row justify-between items-start w-full p-6" style={{zIndex:2}}>
+                    <div className="flex flex-col">
+                        <Image
+                            src = {ShinyHeadshot}
+                            alt="A shiny headshot!"
+                            width={486}
+                            height={486}
+                            className="object-cover z-20 m-3 -mt-64"
+                            />
+                            <iframe data-testid="embed-iframe" 
+                            className="rounded-xl m-3" 
+                            src="https://open.spotify.com/embed/artist/73etH0gcHNOnWLXD0Rbamb?utm_source=generator" 
+                            width="486px" 
+                            height="300px"
+                            frameBorder="0" 
+                            allowFullScreen 
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                            loading="lazy"/>
+                    </div>
+                    <div>
+                        {songs.map((song, i)=>(
+                            <LyricsBox title={song.title} album={song.album} verses={song.verses}/>
+                        ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
-        </>
     )
 }
