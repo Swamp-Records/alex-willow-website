7e8efc94451b278d07241a550d81cdb1fabdf9d1
giorumy <giorumy@gmail.com>
2026-04-15 21:54:26 -0400

    fixed swamp records link on footer!

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full mt-auto px-6 py-4" style={{backgroundColor: "#D8D1C8"}}>
       <p className="text-black font-kurale text-sm">© 2026 Alex Willow. All rights reserved.</p>
       <p className="text-black font-kurale text-sm">
-        Made by <Link href="https://swamprecords.com" target="_blank" className="underline hover:text-darkblue">
+        Made by <Link href="https://swamprecords.org" target="_blank" className="underline hover:text-darkblue">
         Swamp Records
         </Link>
       </p>
